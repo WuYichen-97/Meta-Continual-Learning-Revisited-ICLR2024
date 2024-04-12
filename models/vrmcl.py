0a5a78d201ed8bdf56848df4bc793bbadc4120bb
WuYichen-97 <55070269+WuYichen-97@users.noreply.github.com>
2024-04-11 23:21:13 -0500

    vrmcl.py

--- models/vrmcl.py
+++ models/vrmcl.py
@@ -22,12 +22,9 @@ def get_parser():
     parser.add_argument('--dataset', type=str, required=True, choices=DATASET_NAMES,
                         help='Which dataset to perform experiments on.')
     parser.add_argument('--lr', type=float, required=True, help='Learning rate.')
-
     parser.add_argument('--s_momentum', type=float, required=True, default=0.8, help='momentum_ratio of STORM')
     parser.add_argument('--s_lr', type=float, required=True, default = 0.1, help='learning rate of STORM')
     
-
-
     #parser.add_argument('--optim_wd', type=float, default=0., help='optimizer weight decay.')
     #parser.add_argument('--optim_mom', type=float, default=0., help='optimizer momentum.')
     #parser.add_argument('--optim_nesterov', type=int, default=0, help='optimizer nesterov momentum.')
@@ -81,29 +78,19 @@ class VRMCL(ContinualModel):
         self.fast_weight = None
         self.observed_batch = None
         self.max_batch_in_minitask = None
-        # self.momentum = [] #None
         self.momentum = [[None]]*63
         self.g1 = []
         self.fast_weight_last_step = None
         self.tag = False
         self.task_id = -1
         self.total = -1
-        self.accumu_grad = [[None]]*12
-        self.grad = []#[None]*12
-        self.grad1 = []
-        self.var = []
-        self.var1 = []
-        self.lgrad = [[None]]*12
         self.outer_loss = []
-        self.distance = []
         self.t = 0
-        self.loss_mean =0
-        self.save = 0
 
     def begin_task(self, dataset):
         # clear meta-loss
         self.outer_loss = []
-        self.meta_loss, self.meta_loss1 = [],[]
+        self.meta_loss= []
         self.meta_loss_last_step = []
         # clone the model
         self.fast_weight = self.net.get_fast_weight()
@@ -114,12 +101,10 @@ class VRMCL(ContinualModel):
         self.observed_batch = 0
         self.max_batch_in_minitask = len(dataset.train_loader)
         self.N_CLASSES_PER_TASK = dataset.N_CLASSES_PER_TASK
-        self.iter = 0
         self.task_id = 0
 
     def end_task(self, dataset):
         task_id = dataset.i//dataset.N_CLASSES_PER_TASK 
-        # torch.save(self.outer_loss, 'loss'+str(int(task_id))+'.pth') 
 
 
     def compute_offsets(self, task):
@@ -134,39 +119,38 @@ class VRMCL(ContinualModel):
         loss = self.loss(logits[:, offset1:offset2], y-offset1)
         return loss
     
-    
     def take_multitask_loss(self, bt, logits, y, task_id):
         loss = 0.0
         for i, ti in enumerate(bt):
-            if i < 32:
+            if i < 32:  # 32 is the adopted batchsize 
                 offset1, offset2 = self.compute_offsets(ti)
-                offset2 = 200
-                loss += self.loss(logits[i, offset1:offset2].unsqueeze(0), y[i].unsqueeze(0)-offset1)
+                # offset2 = 200
+                # loss += self.loss(logits[i, offset1:offset2].unsqueeze(0), y[i].unsqueeze(0)-offset1)
+                loss += self.loss(logits[i, offset1:].unsqueeze(0), y[i].unsqueeze(0)-offset1)
             else:
                 _, offset2 = self.compute_offsets(task_id)
-                offset1 = 0
-                loss += 0.4*self.loss(logits[i, offset1:offset2].unsqueeze(0), y[i].unsqueeze(0)-offset1)
+                # offset1 = 0
+                # loss += 0.5*self.loss(logits[i, offset1:offset2].unsqueeze(0), y[i].unsqueeze(0)-offset1)
+                loss += 0.5*self.loss(logits[i, 0:offset2].unsqueeze(0), y[i].unsqueeze(0))
         return loss/len(bt)
 
-    def meta_loss_compute(self, x, y, bt, t, j,y_logits=None):
+    def meta_loss_compute(self, x, y, bt, t, j, y_logits=None):
         """
         differentiate the loss through the network updates wrt alpha
         """
         if j ==0:
             _ = self.net(x) # update the BN part
-        offset1, offset2 = self.compute_offsets(t)
-        logits = self.net.functional_forward(x, fast_weight=self.fast_weight)#[:,:offset2]
+        logits = self.net.functional_forward(x, fast_weight=self.fast_weight)
         loss_q = self.take_multitask_loss(bt, logits, y, t)            
         return loss_q    
 
-    def meta_loss_compute_last_step(self, x, y, bt, t, j,y_logits=None):
+    def meta_loss_compute_last_step(self, x, y, bt, t, j, y_logits=None):
         """
         differentiate the loss through the network updates wrt alpha
         """
-        if j ==0:
+        if j == 0:
             _ = self.net(x) # update the BN part
-        offset1, offset2 = self.compute_offsets(t)
-        logits = self.net.functional_forward(x, fast_weight=self.fast_weight_last_step, mode='ema')#[:,:offset2]
+        logits = self.net.functional_forward(x, fast_weight=self.fast_weight_last_step, mode='ema')
         loss_q = self.take_multitask_loss(bt, logits, y, t)            
         return loss_q   
 
@@ -198,7 +182,7 @@ class VRMCL(ContinualModel):
         correct = torch.eq(pred, y).sum()
         return correct, logits
 
-    def outer_loop(self, task_id):           
+    def outer_loop(self):           
         # update the parameters of backbone
         meta_loss = sum(self.meta_loss) / len(self.meta_loss)
         self.outer_loss.append(meta_loss)
@@ -207,7 +191,6 @@ class VRMCL(ContinualModel):
             torch.nn.utils.clip_grad_norm_(self.net.main.parameters(), self.args.grad_clip_norm)
             torch.nn.utils.clip_grad_norm_(self.task_lr.values(), self.args.grad_clip_norm)
         
-
         meta_loss_last_step = sum(self.meta_loss_last_step) / len(self.meta_loss_last_step)
         meta_loss_last_step.backward()
         if self.args.grad_clip_norm:
@@ -218,12 +201,11 @@ class VRMCL(ContinualModel):
         self.opt_lr.step()
 
         # STORM
-        if self.args.asyn_update:  
+        if self.args.asyn_update: 
             cnt = 0
             with torch.no_grad():
-                        
                 # vr-mcl
-                for g, p, lr in zip(self.net.ema.parameters(), self.net.main.parameters(), self.task_lr.values()):
+                for g, p in zip(self.net.ema.parameters(), self.net.main.parameters()):
                         if self.momentum[cnt][0] == None:
                             p.data = p.data - (p.grad) * self.args.s_lr
                             self.momentum[cnt] = [p.grad]
@@ -231,16 +213,14 @@ class VRMCL(ContinualModel):
                             self.momentum[cnt] = [p.grad + self.args.s_momentum*(self.momentum[cnt][0]-g.grad)]
                             p.data = p.data - (self.momentum[cnt][0]) * self.args.s_lr
                         cnt += 1
-
         else:
             self.opt.step()
         # zero grad and lossl  
         self.opt.zero_grad()
         self.opt_lr.zero_grad()
         self.fast_weight = self.net.get_fast_weight()
-        self.meta_loss, self.meta_loss1 = [], []
+        self.meta_loss = []
         self.meta_loss_last_step = []
-        self.iter+=1 
         return meta_loss
 
 
@@ -257,7 +237,7 @@ class VRMCL(ContinualModel):
                 x_q, y_q = inputs, torch.tensor(labels, dtype=torch.long)
                 buf_id = task_id*torch.ones_like(y_q)
             else:
-                buf_inputs, buf_labels, buf_logits, buf_id = self.buffer.get_data(
+                buf_inputs, buf_labels, buf_id = self.buffer.get_data(
                     self.args.replay_batch_size, transform=self.transform)
                 x_q = torch.cat((inputs, buf_inputs))
                 y_q = torch.cat((torch.tensor(labels,dtype=torch.long), buf_labels))
@@ -268,23 +248,24 @@ class VRMCL(ContinualModel):
                 y = y_s[j * self.args.inner_batch_size:(j + 1) * self.args.inner_batch_size].detach()
                 correct_inner, logits = self.inner_loop(x, y, task_id)
                 _, _ = self.inner_loop_last_step(x, y,task_id)
-                
-                if logits_save == None:
-                    logits_save = logits
-                else:
-                    logits_save = torch.cat((logits_save,logits),dim=0)
+                # if logits_save == None:
+                #     logits_save = logits
+                # else:
+                #     logits_save = torch.cat((logits_save,logits),dim=0)
                 self.meta_loss.append(self.meta_loss_compute(x_q, y_q, buf_id, task_id, j))
                 self.meta_loss_last_step.append(self.meta_loss_compute_last_step(x_q, y_q, buf_id, task_id,j ))
 
                 if not j and not i:
                     correct += correct_inner
+
             # meta_update
-            meta_loss = self.outer_loop(task_id)
+            meta_loss = self.outer_loop()
 
         self.observed_batch += 1
         if self.observed_batch <= self.max_batch_in_minitask:
-            self.buffer.add_data(examples=not_aug_inputs,
-                        labels=labels, logits=logits_save.detach(), task_labels=task_id*torch.ones_like(labels))
+            self.buffer.add_data(examples=not_aug_inputs, logits =None,  labels=labels, task_labels=task_id*torch.ones_like(labels))
+            # self.buffer.add_data(examples=not_aug_inputs,
+            #             labels=labels, logits=logits_save.detach(), task_labels=task_id*torch.ones_like(labels))
 
         return meta_loss.item()
 
